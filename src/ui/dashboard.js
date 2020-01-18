c4744d1aa7bc107c3ac73c4838735388a6d0ca88
Gary Kim <gary@garykim.dev>
2020-01-18 17:50:55 +0800

    Reverse direction

--- src/ui/dashboard.js
+++ src/ui/dashboard.js
@@ -2,11 +2,8 @@ import Feed from './components/Feed';
 import Vue from 'vue';
 
 window.addEventListener('load', () => {
-    setTimeout(() => {
-        const View = Vue.extend(Feed);
-        (new View()).$mount('#app');
-    }, 50);
-
-})
+    const View = Vue.extend(Feed);
+    (new View()).$mount('#app');
+});
 
 
